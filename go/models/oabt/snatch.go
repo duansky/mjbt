4e4858877d5a0ca10a3feda4564b1a0395bdb861
duansky <duansky@gmail.com>
2018-03-27 16:00:40 +0800

    oabt已修改页面代码

--- go/models/oabt/snatch.go
+++ go/models/oabt/snatch.go
@@ -1,8 +1,8 @@
 package oabt
 
 import (
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -16,12 +16,12 @@ func DoSnatch(key string) []*MovieInfo {
 	urlIndex := beego.AppConfig.String("cili001.com")
 
 	if key != "" {
-		urlIndex = urlIndex + "/index?topic_title3=" + url.QueryEscape(key)
+		urlIndex = urlIndex + "/index?k=" + url.QueryEscape(key)
 	}
 	fmt.Println("========url:" + urlIndex)
 
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://" + urlIndex, nil)
+	req, err := http.NewRequest("GET", "http://"+urlIndex, nil)
 	req.Header.Add("Accept", "text/html, application/xhtml+xml, */*")
 	req.Header.Add("Accept-Language", "zh-CN")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36")
@@ -32,43 +32,49 @@ func DoSnatch(key string) []*MovieInfo {
 	//最后直接把res传给goquery就可以来解析网页了
 	doc, err := goquery.NewDocumentFromResponse(res)
 
-//fmt.Println(doc.Find("#pages_btns").Html())
+	//fmt.Println(doc.Find("#pages_btns").Html())
 
 	//	doc, err := goquery.NewDocument(urlIndex)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	listItem := doc.Find(".list-item")
+	listItem := doc.Find(".link-list-wrapper")
 
 	/**********写文件***********/
 	s, _ := doc.Html()
 	ioutil.WriteFile("c:/oabt.html", []byte(s), 0644)
 	/*********************/
-	
+
 	// 不带时间的日期
 	var date string
 	movieInfos := make([]*MovieInfo, 0, 10)
 	listItem.Children().Each(func(i int, n *goquery.Selection) {
 
-		if goquery.NodeName(n) == "dt" {
+		if goquery.NodeName(n) == "p" && n.HasClass("link-list-title") {
 			date = n.Text()
 		}
-		
-		if goquery.NodeName(n) == "dd" {
-			m := new(MovieInfo)
-			m.UpdateTime = date + " " + n.Find(".a").Text()
-			name := strings.Replace(n.Find(".b").Text(), "[CiLi001.com]", "", -1)
-			m.Name = strings.Replace(name, "【ciLi001.com】", "", -1)
-			m.Size = n.Find(".d").Children().First().Text()
-			m.Ed2k, _ = n.Attr("ed2k")
-			m.Magnet, _ = n.Attr("magnet")
-			movieInfos = append(movieInfos, m)
+
+		if goquery.NodeName(n) == "ul" && n.HasClass("link-list") {
+			n.Children().Each(func(i2 int, n2 *goquery.Selection) {
+				if goquery.NodeName(n2) == "li" {
+
+					m := new(MovieInfo)
+
+					m.UpdateTime = date + " " + n2.Find(".time").Text()
+					name := strings.Replace(n2.Find(".name").Text(), "[CiLi001.com]", "", -1)
+					m.Name = strings.Replace(name, "【ciLi001.com】", "", -1)
+					m.Size = n2.Find(".size").Text()
+					m.Ed2k, _ = n2.Attr("data-ed2k")
+					m.Magnet, _ = n2.Attr("data-magnet")
+					movieInfos = append(movieInfos, m)
+				}
+
+			})
+
 		}
-		
+
 	})
-	
-	
 
 	return movieInfos
 }
